5f22c87d0e0cce7c9b9ee6de7489c4ea7f8ffb8a
innovader <70543160+lenintarrillo@users.noreply.github.com>
2022-10-24 08:27:42 -0500

    SWAP 95%
    The implementation of whitelist and limits is pending

--- PachacuySwap.sol
+++ PachacuySwap.sol
@@ -11,7 +11,6 @@
 ///Developer:lenin.tarrillo.v@gmail.com
 ///Bio:https://www.linkedin.com/in/lenintv/
 
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
@@ -21,6 +20,11 @@ import "@openzeppelin/contracts/token/ERC777/IERC777.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/utils/introspection/IERC1820Registry.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC1820Implementer.sol";
+
+//The implementation of whitelist and limits is pending
 
 contract PachacuySwap is
     AccessControl,
@@ -28,19 +32,14 @@ contract PachacuySwap is
     IERC777Sender,
     IERC777Recipient
 {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-    bytes32 public constant COLLECTOR_ROLE = keccak256("COLLECTOR_ROLE");
+    uint256 private _swapPercentageUSDCtoPcuy = 2500 * 1e18; //%
+    uint256 private _swapPercentagePCUYtoUSDC = 3 * 1e18; //%
+    uint256 private _totalUSDCswap;
+    uint256 private _totalPCUYswap;
 
     IERC777 public _PachacuyToken;
     IERC20 public _USDCToken;
-
-    uint256 private _swapRateUSDCtoPcuy;
-    uint256 private _swapRatePCUYtoUSDC;
-    
-
-    uint256 private _totalUsdcSwap;
-    uint256 private _totalPcuySwap;
 
     event TokensReceived(
         address operator,
@@ -60,178 +59,161 @@ contract PachacuySwap is
         bytes operatorData
     );
 
-    event TokenPcuySwap(
-        address Swaper,
-        uint256 usdcAmount,
-        uint256 TokenSwap
-    );
+    event TokenPcuySwap(address Swaper, uint256 usdcAmount, uint256 TokenSwap);
 
-    event TokenUSDCSwap(
-        address Swaper,
-        uint256 pcuyAmount,
-        uint256 TokenSwap
-    );
-    
-
+    event TokenUSDCSwap(address Swaper, uint256 pcuyAmount, uint256 TokenSwap);
 
     bytes32 private constant _TOKENS_SENDER_INTERFACE_HASH =
         keccak256("ERC777TokensSender");
     bytes32 private constant _TOKENS_RECIPIENT_INTERFACE_HASH =
         keccak256("ERC777TokensRecipient");
-        
+
+    IERC1820Registry private _ERC1820_REGISTRY =
+        IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
 
     constructor(address admin) {
-       
         _setupRole(ADMIN_ROLE, admin);
+        _ERC1820_REGISTRY.setInterfaceImplementer(
+            address(this),
+            _TOKENS_RECIPIENT_INTERFACE_HASH,
+            address(this)
+        );
+        _ERC1820_REGISTRY.setInterfaceImplementer(
+            address(this),
+            _TOKENS_SENDER_INTERFACE_HASH,
+            address(this)
+        );
     }
 
-    function totalUsdcSwap() public view returns (uint256) {
-        return _totalUsdcSwap;
+    function totalUSDCswap() public view returns (uint256) {
+        return _totalUSDCswap;
     }
 
-    function totalPcuySwap() public view returns (uint256) {
-        return _totalPcuySwap;
+    function totalPCUYswap() public view returns (uint256) {
+        return _totalPCUYswap;
     }
 
-    
-
-    /**
-     * @dev Lenin Tarrillo
-     * SWAP PCUY to USDC
-     */
+    //SWAP PCUY to USDC
     function swapUSDC_To_PCUY(uint256 _usdcAmount) public whenNotPaused {
-        
-        // verify if approved
         uint256 usdcAllowance = _USDCToken.allowance(
             _msgSender(),
             address(this)
         );
-        
+
         require(
             usdcAllowance >= _usdcAmount,
             "Pachacuy SWAP: Not enough USDC allowance"
         );
-
-        // verify usdc balance
         uint256 usdcBalance = _USDCToken.balanceOf(_msgSender());
         require(
             usdcBalance >= _usdcAmount,
             "Pachacuy SWAP: Not enough USDC balance"
         );
-
-        // transfer usdc to funds wallet
         bool success = _USDCToken.transferFrom(
             _msgSender(),
             address(this),
             _usdcAmount
         );
         require(success, "Pachacuy Swap: Failed to transfer USDC");
-        _totalUsdcSwap += _usdcAmount;
+        _totalUSDCswap += _usdcAmount;
 
-        // total PCUY to transfer
-        uint256 tokenToTransfer = _usdcAmount * _swapRateUSDCtoPcuy;
+        uint256 tokenToTransfer = Math.mulDiv(
+            _usdcAmount,
+            _swapPercentageUSDCtoPcuy,
+            100 * 1e18
+        );
 
-        // verify PCUY balance
         uint256 pcuyBalance = _PachacuyToken.balanceOf(address(this));
         require(
             pcuyBalance >= tokenToTransfer,
             "Pachacuy Swap: Not enough token to Swap"
         );
-
-        // transfer BW3Token to customer
         _PachacuyToken.send(_msgSender(), tokenToTransfer, "");
-        _totalPcuySwap += tokenToTransfer;
-
+        _totalPCUYswap += tokenToTransfer;
         emit TokenPcuySwap(_msgSender(), _usdcAmount, tokenToTransfer);
     }
 
-
-
-
-
-    /**
-     * @dev Lenin Tarrillo
-     * SWAP PCUY to PCUY
-     */
+    //Swap PCUY to USDC
     function swapPCUY_To_USDC(uint256 _pcuyAmount) public whenNotPaused {
-        
-       
-        
         require(
-            _PachacuyToken.isOperatorFor( address(this),_msgSender()),
+            _PachacuyToken.isOperatorFor(address(this), _msgSender()),
             "Pachacuy SWAP: Not enough PCUY allowance"
         );
 
-        // verify usdc balance
         uint256 pcuyBalance = _PachacuyToken.balanceOf(_msgSender());
         require(
             pcuyBalance >= _pcuyAmount,
             "Pachacuy SWAP: Not enough PCUY balance"
         );
 
-        // transfer usdc to funds wallet
         _PachacuyToken.operatorSend(
             _msgSender(),
             address(this),
-            _pcuyAmount, 
+            _pcuyAmount,
             "",
             ""
         );
 
-      
-        _totalPcuySwap += _pcuyAmount;
-
-        // total PCUY to transfer
-        uint256 tokenToTransfer = _pcuyAmount * _swapRateUSDCtoPcuy;
-
-        // verify PCUY balance
+        _totalPCUYswap += _pcuyAmount;
+        uint256 tokenToTransfer = Math.mulDiv(
+            _pcuyAmount,
+            _swapPercentagePCUYtoUSDC,
+            100 * 1e18
+        );
         uint256 usdcBalance = _USDCToken.balanceOf(address(this));
         require(
             usdcBalance >= tokenToTransfer,
             "Pachacuy Swap: Not enough token to Swap"
         );
-
-        // transfer BW3Token to customer
         _USDCToken.transfer(_msgSender(), tokenToTransfer);
-        _totalUsdcSwap += tokenToTransfer;
-
+        _totalUSDCswap += tokenToTransfer;
         emit TokenUSDCSwap(_msgSender(), _pcuyAmount, tokenToTransfer);
     }
 
+    //ADMIN FUNCTIONS
 
-    ////
+    //extracts the entire PCUY balance from the SWAP
+    function getAllBalancePCUY() public whenNotPaused onlyRole(ADMIN_ROLE) {
+        uint256 pcuyBalance = _PachacuyToken.balanceOf(_msgSender());
 
-    function setPCUYaddress(address pcuyAddress)
+        _PachacuyToken.send(_msgSender(), pcuyBalance, "");
+    }
+
+    //extracts the entire PCUY balance from the SWAP
+    function getAllBalanceUSDC() public whenNotPaused onlyRole(ADMIN_ROLE) {
+        uint256 usdcBalance = _USDCToken.balanceOf(_msgSender());
+        _USDCToken.transfer(_msgSender(), usdcBalance);
+    }
+
+    //Set PCUY Token Address
+    function setPCUYTokenAddress(address pcuyAddress)
         public
         onlyRole(ADMIN_ROLE)
     {
         _PachacuyToken = IERC777(pcuyAddress);
     }
 
-    function setUsdcTokenAddress(address uSDCTokenAddress)
+    //Set USDC Token Address
+    function setUSDCTokenAddress(address uSDCTokenAddress)
         public
         onlyRole(ADMIN_ROLE)
     {
         _USDCToken = IERC20(uSDCTokenAddress);
     }
 
-    function swapRateUSDCtoPcuy(uint256 amount)
+    function swapPercentageUSDCtoPcuy(uint256 amount)
         public
         onlyRole(ADMIN_ROLE)
     {
-        _swapRateUSDCtoPcuy = amount;
+        _swapPercentageUSDCtoPcuy = amount;
     }
 
-    function swapRatePCUYtoUSDC(uint256 amount)
+    function swapPercentagePCUYtoUSDC(uint256 amount)
         public
         onlyRole(ADMIN_ROLE)
     {
-        _swapRatePCUYtoUSDC = amount;
+        _swapPercentagePCUYtoUSDC = amount;
     }
-
-
-
-    ////STARDART FUNCTIONS
 
     function pause() public onlyRole(ADMIN_ROLE) {
         _pause();
